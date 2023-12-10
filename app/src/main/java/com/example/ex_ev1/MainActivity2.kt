c06940dd6c0399943bfbe525bd4da21cd9b19085
belen579 <belenbastosdavila@hotmail.com>
2023-12-10 23:09:37 +0100

    Cambios en el formulario

--- app/src/main/java/com/example/ex_ev1/MainActivity2.kt
+++ app/src/main/java/com/example/ex_ev1/MainActivity2.kt
@@ -14,11 +14,15 @@ class MainActivity2 : AppCompatActivity() {
 
         val intent = intent
         val textorecibido = intent?.getSerializableExtra("Formulario")as? MainActivity.Formulario
+        val nombre=getString( R.string.nombre)
+        val apellidos =getString(R.string.apellidos)
+        val contraseña = getString(R.string.contraseña)
+        val titulo =getString(R.string.titulo)
 
         val textView = findViewById<TextView>(R.id.finaltextview)
         if (textorecibido != null) {
-            textView.text="Nombre :"+textorecibido.nombre.toString() +"\n" + "Apellidos :"+ textorecibido.apellidos.toString()+"\n"+ "Email :"+ textorecibido.email.toString()+"\n"+
-                    "Contraseña :" +textorecibido.contraseña.toString()+"\n" +"Titulo :" +textorecibido.titulo.toString()
+            textView.text="$nombre :"+ textorecibido.nombre.toString() +"\n" + "$apellidos :"+ textorecibido.apellidos.toString()+"\n"+ "Email :"+ textorecibido.email.toString()+"\n"+
+                    "$contraseña :" +textorecibido.contraseña.toString()+"\n" +"$titulo :" +textorecibido.titulo.toString()
         }
     }
 }
